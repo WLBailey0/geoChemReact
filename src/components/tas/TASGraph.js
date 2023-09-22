4ad9fe1c35b528bc239a18876d679dbd83080090
William Bailey <WLBailey0@gmail.com>
2023-09-21 22:53:21 -0500

    dynamicly update tas graph with user data

---
+++ src/components/tas/TASGraph.js
@@ -0,0 +1,258 @@
+import React from "react";
+import CanvasJSReact from '@canvasjs/react-charts';
+
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+function TasGraph({chemData}) {
+  
+  function formatGraphData(data){
+    let formatted = [];
+    data.forEach(formatted.push({x: parseInt(data[i].si), 
+      y: parseInt(data[i].kna)}))
+    // for (let i = 0; i < data.length; i++){
+      // formatted.push({x: parseInt(data[i].si), 
+      //   y: parseInt(data[i].kna)})
+    // }
+    return formatted;
+  };
+  const options = {
+    title: {
+      text: "TAS Plot"
+    },
+    axisY: {
+      minimum: 0,
+      maximum: 18
+    },
+    axisX: {
+      minimum: 34,
+      maximum: 85
+    },
+
+    data: [    
+      {
+        type: "scatter",
+        dataPoints: 
+          formatGraphData(chemData)
+        
+      },    
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:41, y: 1}, 
+          {x: 41, y:7}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:45, y: 1}, 
+          {x: 45, y:5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:52, y: 1}, 
+          {x: 52,y:5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:57, y: 1}, 
+          {x: 57,y:6}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:63, y: 1}, 
+          {x: 63,y:7}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:77, y: 1}, 
+          {x: 69, y:8}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:69, y: 8}, 
+          {x: 69, y: 13}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:63, y: 7}, 
+          {x: 69, y: 8}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:45, y: 5}, 
+          {x: 52, y: 5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:52, y: 5}, 
+          {x: 57,y: 6}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:63, y: 7}, 
+          {x: 57,y: 6}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:41, y: 7}, 
+          {x: 45, y: 9}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:45, y: 9}, 
+          {x: 49, y: 11.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:52, y: 14}, 
+          {x: 49, y:11.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:50, y: 15}, 
+          {x: 52, y:14}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x: 52, y: 14}, 
+          {x: 57.5, y:11.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x: 60, y: 13}, 
+          {x: 57.5, y:11.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x: 45, y: 9}, 
+          {x: 49, y:7}]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x: 49, y: 7}, 
+          {x: 52, y:5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x: 45, y: 5}, 
+          {x: 49, y:7}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:49, y:11.5}, 
+          {x: 53, y: 9.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:49, y: 7}, 
+          {x: 53, y: 9.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:53, y: 9.5}, 
+          {x: 57.5, y: 11.5}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:57.5 , y: 11.5}, 
+          {x: 63, y: 7}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:53, y: 9.5}, 
+          {x: 57, y: 6}
+        ]
+      },
+      {
+        lineColor: "black",
+        type: "line",
+        dataPoints: [
+          {x:41, y: 3}, 
+          {x: 45, y: 3}
+        ]
+      }
+    ]
+  }
+  
+  return (
+    <div>
+      <CanvasJSChart options = {options}
+      containerProps={{ width: '33%', height: '33%' }}
+        /* onRef = {ref => this.chart = ref} */
+      />
+      </div>
+  );
+}
+
+export default TasGraph;
---
+++ src/components/tas/form-data-component.js
@@ -0,0 +1,55 @@
+
+import { useState } from 'react';
+import TasGraph from './TASGraph';
+
+let nextId = 0;
+
+export default function List() {
+  const [si, setSi] = useState('');
+  const [kna, setKna] = useState('');
+  const [valueInput, setValueInput] = useState([]);
+  const [active, setActive] = useState(false)
+  
+
+
+  return (
+    <>
+      <h1>Value Input:</h1>
+      <label for='si'>SiO2</label>
+      <input
+        placeholder='SiO2'
+        value={si}
+        onChange={e => setSi(e.target.value)}
+      />&nbsp;
+      <label for='nak'>K2O+Na2O</label>
+      <input
+      placeholder='K2O+Na2O'
+      value={kna}
+      onChange={e => setKna(e.target.value)}
+      />
+      <button onClick={() => {
+        setValueInput([
+          ...valueInput,
+          { id: nextId++, si: si, kna: kna}
+        ]);
+      }}>Add</button>
+      <ul>
+        {/* <th>SiO<sub>2</sub></th> | <th>K<sub>2</sub>O + Na<sub>2</sub>O</th> */}
+        {valueInput.map(value => (
+          <li key={value.id}>
+            {value.si}, {value.kna}
+            </li>
+        ))}
+      </ul>
+      <button onClick={() => 
+        setActive(true)}
+        >Graph</button>
+      <div>
+        {active && <TasGraph chemData={valueInput}/>}
+      </div>
+
+    </>
+  );
+}
+
+
