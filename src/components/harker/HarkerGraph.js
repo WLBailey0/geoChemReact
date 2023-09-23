a212d2249a2e3700b658aab5aaadb98b6a55bcc5
William Bailey <WLBailey0@gmail.com>
2023-09-23 15:19:24 -0500

    Framework for harkers in place

---
+++ src/components/harker/HarkerGraph.js
@@ -0,0 +1,11 @@
+// import CanvasJSReact from '@canvasjs/react-charts';
+
+function HarkerPlot({data}){
+
+    return(
+        <div>
+            <h1>Harker Graphs {data}</h1>
+        </div>
+    )
+}
+export default HarkerPlot;
--- src/components/harker/harker-form-component.js
+++ src/components/harker/harker-form-component.js
@@ -1,18 +1,33 @@
 import {useState} from "react";
+import HarkerPlot from './HarkerGraph';
 
 function Harker() {
-    const [si, setSi] = useState('');
+    // const [si, setSi] = useState('');
+    const [active, setActive] = useState(false);
 
     return (
-        <div>
-            <h1>Harker Plots</h1>
-            <label for="si">SiO<sub>2</sub></label>
-            <input 
-              placeholder="SiO2"
-              value={si}
-              onchange={e => setSi(e.target.value)}
-            />
-        </div>
+        <>
+            <div>
+                <h1>Harker Plots</h1>
+                {/* <label for="si">SiO<sub>2</sub></label>
+                <input 
+                placeholder="SiO2"
+                value={si}
+                onchange={e => setSi(e.target.value)}
+                /> */}
+                <button onClick={() => 
+                    setActive(true)}>
+                    Harker Diagrams
+                </button>
+            </div>
+            <div>
+                {active && <HarkerPlot data={"FeO"} />}
+                {active && <HarkerPlot data={"Al2O3"} />}
+                {active && <HarkerPlot data={"MgO"} />}
+                {active && <HarkerPlot data={"K2O"} />}
+                {active && <HarkerPlot data={"TiO2"} />}
+            </div>
+        </>
     )
 };
 export default Harker;
